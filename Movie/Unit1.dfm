f8aabc1354fe68f57fa6a43271412de980337dc7
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2024-04-09 22:44:58 +0300

    List in description
    Form1.onCreate initializes cartoon and create frames order
    PersonSecondFrame - the lowest position of stickman arms.
    New way of storing frames: in array of TFrame, where TFrame is procedure-type for PersonNthFrame.
    DrawBackground() Create background after pressing Button2

--- Movie/Unit1.dfm
+++ Movie/Unit1.dfm
@@ -3,26 +3,27 @@ object Form1: TForm1
   Top = 0
   BorderStyle = bsToolWindow
   Caption = 'Form1'
-  ClientHeight = 731
-  ClientWidth = 1032
+  ClientHeight = 693
+  ClientWidth = 1272
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  OnCreate = FormCreate
   TextHeight = 15
   object Button1: TButton
-    Left = 784
-    Top = 589
+    Left = 936
+    Top = 581
     Width = 75
     Height = 25
-    Caption = 'Button1'
+    Caption = 'Spawn Stick'
     TabOrder = 0
     OnClick = Button1Click
   end
   object Edit1: TEdit
-    Left = 784
+    Left = 936
     Top = 494
     Width = 121
     Height = 23
@@ -30,7 +31,7 @@ object Form1: TForm1
     Text = '100'
   end
   object Edit2: TEdit
-    Left = 784
+    Left = 936
     Top = 523
     Width = 121
     Height = 23
@@ -38,14 +39,24 @@ object Form1: TForm1
     Text = '100'
   end
   object Edit3: TEdit
-    Left = 784
+    Left = 936
     Top = 552
     Width = 121
     Height = 23
     TabOrder = 3
     Text = '10'
   end
+  object Button2: TButton
+    Left = 1024
+    Top = 581
+    Width = 75
+    Height = 25
+    Caption = 'Button2'
+    TabOrder = 4
+    OnClick = Button2Click
+  end
   object Timer1: TTimer
+    Enabled = False
     OnTimer = Timer1Timer
     Left = 832
     Top = 216
--- Movie/Unit1.pas
+++ Movie/Unit1.pas
@@ -13,8 +13,11 @@ type
     Edit2: TEdit;
     Edit3: TEdit;
     Timer1: TTimer;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,6 +36,9 @@ type
     Head: TRect;
   end;
 
+  TFrame = procedure(const currPerson: TPerson);
+  TFrames = array of TFrame;
+
 var
   Form1: TForm1;
 
@@ -42,6 +48,22 @@ implementation
 
 var
   FirstPerson: TPerson;
+  cartoon: TFrames;
+  framesCount: integer;
+  currFrame: integer;
+
+procedure DrawBackground();
+begin
+    Form1.Canvas.Pen.Width := 3;
+
+    Form1.Canvas.MoveTo(400, 25);
+    Form1.Canvas.LineTo(100, 680);
+    Form1.Canvas.MoveTo(800, 25);
+    Form1.Canvas.LineTo(1100, 680);
+    Form1.Canvas.Rectangle(60, 500, 100, 680);
+    Form1.Canvas.Rectangle(1100, 500, 1140, 680);
+    Form1.Canvas.Rectangle(60, 540, 1140, 640);
+end;
 
 function ConstructorPerson(const X, Y, Size: integer): TPerson;
 var
@@ -63,12 +85,38 @@ begin
     Body[2].Create(X, Y + 2 * Size);
 
     ArmLeft[1].Create(X, Y);
-    ArmLeft[2].Create(X - Size, Y);
-    ArmLeft[3].Create(X - trunc(1.5 * Size), Y - Size);
+    ArmLeft[2].Create(X - Size, Y-size);
+    ArmLeft[3].Create(X - trunc(1.5 * Size), Y - 2*Size);
 
     ArmRight[1].Create(X, Y);
-    ArmRight[2].Create(X + Size, Y);
-    ArmRight[3].Create(X + trunc(1.5 * Size), Y - Size);
+    ArmRight[2].Create(X + Size, Y-size);
+    ArmRight[3].Create(X + trunc(1.5 * Size), Y - 2*Size);
+
+    LegLeft[1].Create(X, Y + 2 * Size);
+    LegLeft[2].Create(X - Size, Y + 3 * Size);
+    LegLeft[3].Create(X - Size, Y + 4 * Size);
+
+    LegRight[1].Create(X, Y + 2 * Size);
+    LegRight[2].Create(X + Size, Y + 3 * Size);
+    LegRight[3].Create(X + Size, Y + 4 * Size);
+  end;
+end;
+
+procedure PersonSecondFrame(const CurrPerson: TPerson);
+begin
+  with CurrPerson, Center do
+  begin
+    Head.Create(X - Size, Y - 3 * Size, X + Size, Y - 1 * Size);
+    Body[1].Create(X, Y - Size);
+    Body[2].Create(X, Y + 2 * Size);
+
+    ArmLeft[1].Create(X, Y);
+    ArmLeft[2].Create(X - Size, Y+size);
+    ArmLeft[3].Create(X - trunc(1.5 * Size), Y + 2 * Size);
+
+    ArmRight[1].Create(X, Y);
+    ArmRight[2].Create(X + Size, Y+size);
+    ArmRight[3].Create(X + trunc(1.5 * Size), Y+2*Size);
 
     LegLeft[1].Create(X, Y + 2 * Size);
     LegLeft[2].Create(X - Size, Y + 3 * Size);
@@ -97,11 +145,36 @@ end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
+  currFrame := 0;
   FirstPerson := ConstructorPerson(StrToInt(Edit1.Text), StrToInt(Edit2.Text), StrToInt(Edit3.Text));
   PersonFirstFrame(FirstPerson);
   DrawPerson(FirstPerson);
+
   Timer1.Enabled := True;
-  Timer1.Interval := 30;
+  Timer1.Interval := 100;
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+    DrawBackground();
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+    framesCount := 30;
+    currFrame := 0;
+
+    SetLength(cartoon, framesCount);
+
+    for var i := Low(cartoon) to High(cartoon) do
+    begin
+        if (i mod 2 = 0) then
+            cartoon[i] := PersonFirstFrame
+        else
+            cartoon[i] := PersonSecondFrame;
+    end;
+
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
@@ -109,10 +182,16 @@ begin
   Form1.Canvas.Pen.Mode := pmNotXor;
   DrawPerson(FirstPerson);
   with FirstPerson do
-    FirstPerson := ConstructorPerson(Center.X + 10, Center.Y + 10, Size + 1);
-  PersonFirstFrame(FirstPerson);
-  DrawPerson(FirstPerson);
+    FirstPerson := ConstructorPerson(Center.X + 5, Center.Y + 10, Size + 1);
 
+  if currFrame >= framesCount-1 then
+    Timer1.Enabled := false
+  else
+  begin
+    cartoon[currFrame](FirstPerson);
+    DrawPerson(FirstPerson);
+    inc(currFrame);
+  end;
 end;
 
 end.
